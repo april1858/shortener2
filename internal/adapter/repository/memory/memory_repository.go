01eb29cbb33c9b327fa025062d97eb4975c54507
april1858 <april1858@yandex.ru>
2023-01-23 20:19:27 +0700

    with mutex

--- internal/adapter/repository/memory/memory_repository.go
+++ internal/adapter/repository/memory/memory_repository.go
@@ -1,11 +1,15 @@
 package memory
 
 import (
+	"sync"
+
 	"github.com/april1858/shortener2/internal/app/entity"
 )
 
 type Repository struct {
 	memory map[string]string
+	rwLock *sync.RWMutex
+	mx     *sync.RWMutex
 }
 
 func NewRepository() *Repository {
@@ -14,10 +18,16 @@ func NewRepository() *Repository {
 }
 
 func (mr Repository) Store(redirect *entity.Redirect) {
+	mr.mx = new(sync.RWMutex)
+	mr.mx.Lock()
 	mr.memory[redirect.ShortURL] = redirect.OriginalURL
+	mr.mx.Unlock()
 }
 
 func (mr Repository) Find(code string) string {
+	mr.rwLock = new(sync.RWMutex)
+	mr.rwLock.Lock()
+	defer mr.rwLock.Unlock()
 	outURL := mr.memory[code]
 	return outURL
 }
